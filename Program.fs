01f7357072e0f931d88df195bfa4237af0428943
David Ungar <dungar@dottys.com>
2023-12-02 19:47:04 -0800

    explaining why the program.fs is useless

--- Program.fs
+++ Program.fs
@@ -1,20 +1,2 @@
-﻿open System.IO
-open System
-
-let inputData =  File.ReadLines @".\Day1\input.txt"
-
-let getNumberFromString (input:string) = 
-    input.Trim()
-    |> Seq.filter(fun letter ->
-        printfn "%A" letter
-        Char.IsNumber(letter)
-    ) 
-    |> fun numbers  -> Seq.head numbers + Seq.last numbers 
-    |> fun number -> Char.ToString(number) |> int
-
-let result =
-    inputData
-    |> Seq.map getNumberFromString
-    // |> Seq.sum
-
-result
+﻿// IGNORE ME
+// fsx files in the various Day folders.  
